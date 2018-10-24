7bdb55538ca412613dfd2a46f4734127c7e187aa
Brian O'Leary <brian.oleary15@gmail.com>
2018-10-24 15:57:51 -0400

    Second Commit

---
+++ trackingTutorsIOSTests/trackingTutorsIOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  trackingTutorsIOSTests.swift
+//  trackingTutorsIOSTests
+//
+//  Created by Brian Thomas O'Leary on 10/12/18.
+//  Copyright © 2018 Brian Thomas O'Leary. All rights reserved.
+//
+
+import XCTest
+@testable import trackingTutorsIOS
+
+class trackingTutorsIOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
